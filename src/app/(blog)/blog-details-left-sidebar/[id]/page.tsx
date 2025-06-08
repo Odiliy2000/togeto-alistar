3bed11b5f56a0c02b4b1e5c451a0ca33a7b858d3
Odiliy2000 <updatedepartment@alistarincoh.com>
2025-06-08 16:02:39 -0400

    Fix blog dynamic route typing for Netlify static build

--- src/app/(blog)/blog-details-left-sidebar/[id]/page.tsx
+++ src/app/(blog)/blog-details-left-sidebar/[id]/page.tsx
@@ -2,28 +2,29 @@ import { Metadata } from 'next';
 import { blogData } from '@/data/blog-data';
 import BlogDetailsLeftSidebarMain from '@/views/blog-details/blog-details-left-sidebar';
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
+type Params = { id: string };
 
-export async function generateMetadata({
-  params,
-}: PageProps): Promise<Metadata> {
+export async function generateMetadata(
+  props: { params: Params }
+): Promise<Metadata> {
+  const { params } = props;
   const blog = blogData.find((blog) => String(blog.id) === params.id);
   return {
     title: blog ? `Alistar - ${blog.title}` : 'Alistar - Blog Not Found',
   };
 }
 
-export async function generateStaticParams(): Promise<{ id: string }[]> {
+export async function generateStaticParams(): Promise<Params[]> {
   return blogData.map((blog) => ({
     id: String(blog.id),
   }));
 }
 
-export default function BlogDetailsLeftSidebarPage({ params }: PageProps) {
+export default function BlogDetailsLeftSidebarPage({
+  params,
+}: {
+  params: Params;
+}) {
   const blog = blogData.find((blog) => String(blog.id) === params.id);
   return blog ? (
     <BlogDetailsLeftSidebarMain blog={blog} />
